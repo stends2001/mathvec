63a41b3e50dbf742f021b1b60ee2de23c590845d
stends2001 <stends2001@gmail.com>
2026-07-01 12:34:56 +0200

    updated pathmanager

--- src/backend/pathmanager.py
+++ src/backend/pathmanager.py
@@ -3,33 +3,20 @@ from pathlib import Path
 class PathNotFound(Exception):
 
     def __init__(self, pathname: str, path: Path):
-        msg = f'Path {pathname} not found at {path}'
+        msg = f'path {pathname} not found at {path}'
         super().__init__(msg)
 
 class PathManager:
-    """
-    Path Manager class for the entire project
-
-    Attributes
-    ----------
-    - `project`
-    - `src`
-    - `config`
-    - `assets`
-    - `output`
-    """
     def __init__(self):
         self.project = Path(__file__).parents[2]
-        self.src     = self.project / 'src'       
-        self.config  = self.project / 'config'    
-        self.assets  = self.project / 'assets'             
-        self.output  = self.project / 'output'
+        self.src     = self.project / 'src'
+        self.assets  = self.project / 'assets'
+        self.output  = self.project / 'output'        
 
-        self._assert_path_existence()
+        self._assert_path_integrity()
 
-    def _assert_path_existence(self):
-        path: Path
+    def _assert_path_integrity(self):
+        path: Path 
         for pathname, path in vars(self).items():
             if not path.exists():
                 raise PathNotFound(pathname, path)
-
