a2b29d253cd1648a347b3caa05f51b784f64ab43
stends2001 <stends2001@gmail.com>
2026-09-05 11:33:12 +0200

    updated docstrings

--- src/frontend/buttonmanager.py
+++ src/frontend/buttonmanager.py
@@ -59,7 +59,7 @@ def get_buttonstyle(latex_status : bool | None,
 
 class _ButtonProtocol(Protocol):
     """
-    Protocol that lists methods in MathVecApp, 
+    Protocol that lists methods in ``MathVecApp``
     as fallback for ButtonManagerMixin
     """
     latex_supported : bool
