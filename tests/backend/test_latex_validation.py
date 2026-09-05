b0c253f2221efdc334970b04ee4eab8e1e88726c
stends2001 <stends2001@gmail.com>
2026-09-05 11:04:51 +0200

    removed tests

--- tests/backend/test_latex_validation.py
+++
@@ -1,6 +0,0 @@
-from src.backend.latex_validation import has_latex_package
-
-def test_mkdir_creates_output_and_history():
-
-    assert has_latex_package('amsmath'), "No 'amsmath' LaTeX-package found."
-    assert not has_latex_package('amsmat'),  "LaTeX-package 'amsmat' not supposed to exist."
